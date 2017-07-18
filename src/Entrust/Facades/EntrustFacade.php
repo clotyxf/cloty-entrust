841667f59ebf122eb7f1f7f3f61fc028cad04b6d
cloty <291555677@qq.com>
2017-07-18 12:49:32 +0800

    facades

---
+++ src/Entrust/Facades/EntrustFacade.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cloty\Entrust\Facades;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Cloty\Entrust
+ */
+
+use Illuminate\Support\Facades\Facade;
+
+class EntrustFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'entrust';
+    }
+}
