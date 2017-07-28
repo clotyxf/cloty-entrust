f4d6fb276bbd0fcd2137d6a8725ec56f01ff100a
cloty <291555677@qq.com>
2017-07-28 19:21:44 +0800

    fix:,

--- src/config/cloty-entrust.php
+++ src/config/cloty-entrust.php
@@ -152,7 +152,7 @@ return [
     | Supported: "apc", "array", "memcached", "redis"
     |
     */
-    'cache_driver' => env('ENTRUST_CACHE_DRIVER', 'file'),,
+    'cache_driver' => env('ENTRUST_CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
