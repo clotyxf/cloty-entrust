377fb6d054cc83c6037bbf2036f231af71675ef6
cloty <291555677@qq.com>
2017-07-29 16:12:26 +0800

    fix:use

--- src/Entrust/EntrustPermission.php
+++ src/Entrust/EntrustPermission.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class EntrustPermission extends Model
 {
-    use \Cloty\Entrust\Traits\User\PermissionTrait;
+    use \Cloty\Entrust\Traits\PermissionTrait;
     /**
      * Table name.
      *
